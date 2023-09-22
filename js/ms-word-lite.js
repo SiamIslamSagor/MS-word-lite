ba614494b7b8ad462f24d963cc6428bcb2194e57
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-23 01:59:00 +0600

    added togle btn feature in project

--- js/ms-word-lite.js
+++ js/ms-word-lite.js
@@ -1,54 +1,83 @@
-document.getElementById("btn-bold").addEventListener("click", function () {
-  document.getElementById("text-area").style.fontWeight = "bold";
-});
-document.getElementById("btn-italic").addEventListener("click", function () {
-  document.getElementById("text-area").style.fontStyle = "italic";
-});
-document.getElementById("btn-underline").addEventListener("click", function () {
-  document.getElementById("text-area").style.textDecoration = "underline";
+/////////////////////////////////////////////////////////////////////////
+
+const textArea = document.getElementById("text-area");
+const boldBtn = document.getElementById("btn-bold");
+let boldBtnValue = false;
+
+const italicBtn = document.getElementById("btn-italic");
+let italicBtnValue = false;
+
+const underlineBtn = document.getElementById("btn-underline");
+let underlineBtnValue = false;
+
+const leftBtn = document.getElementById("btn-align-left");
+let leftBtnValue = false;
+
+const centerBtn = document.getElementById("btn-align-center");
+let centerBtnValue = false;
+
+const rightBtn = document.getElementById("btn-align-right");
+let rightBtnValue = false;
+
+const justifyBtn = document.getElementById("btn-align-justify");
+let justifyBtnValue = false;
+
+const upperCaseBtn = document.getElementById("btn-to-upper-case");
+let upperCaseBtnValue = false;
+
+/////////////////////////////////////////////////////////////////////////
+
+boldBtn.addEventListener("click", function () {
+  boldBtnValue = !boldBtnValue;
+  boldBtnValue
+    ? (textArea.style.fontWeight = "bold")
+    : (textArea.style.fontWeight = "normal");
 });
 
-document
-  .getElementById("btn-align-left")
-  .addEventListener("click", function () {
-    // document.getElementById('text-area').style.fontWeight = 'bold';
-    document.getElementById("text-area").style.textAlign = "left";
-  });
-document
-  .getElementById("btn-align-center")
-  .addEventListener("click", function () {
-    document.getElementById("text-area").style.textAlign = "center";
-  });
-document
-  .getElementById("btn-align-right")
-  .addEventListener("click", function () {
-    document.getElementById("text-area").style.textAlign = "right";
-  });
-document
-  .getElementById("btn-align-justify")
-  .addEventListener("click", function () {
-    document.getElementById("text-area").style.textAlign = "justify";
-  });
+italicBtn.addEventListener("click", function () {
+  italicBtnValue = !italicBtnValue;
+  italicBtnValue
+    ? (textArea.style.fontStyle = "italic")
+    : (textArea.style.fontStyle = "normal");
+});
 
-document
-  .getElementById("btn-to-upper-case")
-  .addEventListener("click", function () {
-    const textArea = document.getElementById("text-area");
-    const toUpperCaseText = textArea.value.toUpperCase();
-    textArea.value = toUpperCaseText;
-  });
+underlineBtn.addEventListener("click", function () {
+  underlineBtnValue = !underlineBtnValue;
+  underlineBtnValue
+    ? (textArea.style.textDecoration = "underline")
+    : textArea.style.removeProperty("text-decoration");
+});
+
+leftBtn.addEventListener("click", function () {
+  textArea.style.textAlign = "left";
+});
+centerBtn.addEventListener("click", function () {
+  textArea.style.textAlign = "center";
+});
+rightBtn.addEventListener("click", function () {
+  textArea.style.textAlign = "right";
+});
+justifyBtn.addEventListener("click", function () {
+  textArea.style.textAlign = "justify";
+});
+
+upperCaseBtn.addEventListener("click", function () {
+  const upper = textArea.value.toUpperCase();
+  const lower = textArea.value.toLowerCase();
+
+  upperCaseBtnValue = !upperCaseBtnValue;
+  upperCaseBtnValue ? (textArea.value = upper) : (textArea.value = lower);
+});
 
 function colorStyle(clickedElementId) {
-  if (clickedElementId === "favcolor") {
+  if (clickedElementId === "fav-color") {
     const colorCode = document.getElementById(clickedElementId).value;
     document.getElementById("text-area").style.color = colorCode;
   }
 
   if (clickedElementId === "change-font-size") {
     const numberValue = document.getElementById(clickedElementId).value;
-    console.log(numberValue);
     const fontSize = numberValue + "px";
-    console.log(fontSize);
     document.getElementById("text-area").style.fontSize = fontSize;
   }
 }
